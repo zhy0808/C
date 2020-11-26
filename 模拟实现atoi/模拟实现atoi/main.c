dcc705c959aa746c253ed27f080756226d65bd29
zhy0808 <2423985691@qq.com>
2020-11-26 16:01:28 +0800

    2020.11.26 my_atoi(将字符串转化成整数型)

---
+++ 模拟实现atoi/模拟实现atoi/main.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<windows.h>
+
+int my_atoi(char *a)
+{
+	int b = 0;
+	int c = 1;
+	if (*a == ' ')
+		a++;
+	if (*a == '-')
+	{
+		c = -1;
+		a++;
+	}
+	while (*a >= '0' && *a <= '9')
+	{
+		b = b * 10 + (*a - '0');
+		a++;
+	}
+	return b*c;
+}
+int main()
+{
+	char *a = "-123789";
+	my_atoi(a);
+	printf("%d\n",my_atoi(a));
+	system("pause");
+	return 0;
+}
